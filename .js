1cf9e83c535dfa3a0914ddf722d9a20101d30be0
JB <lemon.yogurt@tutanota.com>
2026-01-03 18:13:21 +0100

    update

---
+++ .js
@@ -0,0 +1,66 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+// Define folders
+const rootDir = __dirname;
+const mdDir = path.join(rootDir, 'songs_md');
+const songsDir = path.join(rootDir, 'songs');
+const pdfDir = path.join(rootDir, 'pdf');
+
+// Ensure folders exist
+for (const dir of [mdDir, songsDir, pdfDir]) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+}
+
+// Get all .md files from songs_md
+const mdFiles = fs.readdirSync(mdDir).filter(file => file.endsWith('.md'));
+
+// Function to execute a command and return a Promise
+function runCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing command: ${command}`);
+        console.error(stderr);
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+async function main() {
+  try {
+    // Convert all .md files to HTML
+    for (const file of mdFiles) {
+      const mdPath = path.join(mdDir, file);
+      const outFile = path.basename(file, ".md") + ".html";
+      const outPath = path.join(songsDir, outFile);
+      console.log(`Converting ${file} to HTML...`);
+      await runCommand(`node md_to_html.js "${mdPath}" "${outPath}"`);
+    }
+
+    // Move all .html files to the 'songs' folder
+    //const htmlFiles = fs.readdirSync(rootDir).filter(file => file.endsWith('.html'));
+    //for (const file of htmlFiles) {
+    //  const oldPath = path.join(rootDir, file);
+    //  const newPath = path.join(songsDir, file);
+    //  fs.renameSync(oldPath, newPath);
+    //  console.log(`Moved ${file} to songs/`);
+    //}
+
+    // Run indexing.js (keeps index.html in rootDir)
+    console.log('Running indexing.js...');
+    await runCommand('node indexing.js');
+
+    console.log('All tasks completed successfully!');
+  } catch (err) {
+    console.error('An error occurred:', err);
+  }
+}
+
+main();
---
+++ to_do/tender.md
@@ -0,0 +1,70 @@
+# Tender (Cric crac) - Blur
+
+## Accords:
+<uke-chord name="A" frets="2100"></uke-chord> 
+<uke-chord name="G" frets="0232"></uke-chord> 
+<uke-chord name="D" frets="2220"></uke-chord> 
+<uke-chord name="C" frets="0003"></uke-chord> 
+<uke-chord name="C#m" frets="1104"></uke-chord> 
+<uke-chord name="E7" frets="1202"></uke-chord> 
+
+
+## Instructions:
+Le premier [A] du refrain correspond à la fin du pont.  
+Intro fingerstyle donnée:
+
+musicxml: ./tender_intro.musicxml
+
+## Paroles:
+
+### Intro
+[A A A E7 A]
+[A A A E7 A]
+
+ 
+
+### Couplet 1
+[A]Tender is the night  
+[(G)]Lying by your [(A)]side  
+[A]Tender is the touch  
+Of [(G)]someone that you [(A)]love too much  
+[A]Tender is the day  
+The [(G)]demons go [(A)]away  
+[A]Lord I need to find  
+[(G)]Someone who can [(A)]heal my mind  
+
+:::highlight
+### Pont 
+[A]Come on, Come on, Come on [(D)]Get [(C)]through it  
+[A]Come on, Come on, Come on [(C#m)]Love's the [(D)]greatest thing  
+[A]Come on, Come on, Come on [(D)]Get [(C)]through it  
+[A]Come on, Come on, Come on [(C#m)]Love's the [(D)]greatest thing  
+[(C#m)]That we [(D)]have  
+[(C#m)]I'm waiting for that [(D)]feeling  
+[(C#m)]Waiting for that [(D)]feeling  
+[(A)]Waiting for that [(D)]feeling to [A]come  
+
+### Refrain
+Oh my [A]baby, Oh my [A]baby, Oh [E7]why, Oh [A]my  
+Oh my [A]baby, Oh my [A]baby, Oh [E7]why, Oh [A]my  
+:::
+
+### Couplet 2
+[A]Tender is the ghost  
+The [(G)]ghost I love the [(A)]most  
+[A]Hiding from the sun  
+[(G)]Waiting for the [(A)]night to come  
+[A]Tender is my heart  
+I'm [(G)]screwing up my [(A)]life  
+[A]Lord I need to find  
+[(G)]Someone who can [(A)]heal my mind  
+
+:::highlight
+### Pont/refrain X2
+:::
+
+### Couplet 1
+
+:::highlight
+### Pont/refrain
+:::
